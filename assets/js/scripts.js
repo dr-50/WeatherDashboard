a12e29b2c4bc7d95f60b7f2a59e11154c0b593be
Devon <devonross50@yahoo.com>
2021-05-08 20:38:33 -0500

    start of 5 day forecast complete. Converted Kelvin to fahrenheit on console log

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,8 +6,9 @@ var formSubmitHandler = function(event){
     var cityname = cityNameInputEl.value.trim();
 
     if(cityname){
-        getCityWeather(cityname);
-        cityNameInputEl.value="";
+        //getCityWeather(cityname);
+        //cityNameInputEl.value="";
+        getCityWeatherFive(cityname);
     } else {
         alert("Please enter a city");
     }
@@ -23,6 +24,43 @@ var getCityWeather = function(city) {
             console.log(response);
             response.json().then(function(data){
                 console.log(data);
+                //return data for first card
+                console.log(data.name);
+                     //return temp
+                console.log(data.main.temp);
+                    //return wind
+                console.log(data.wind.speed);
+                    //return humidity
+                console.log(data.main.humidity);
+                    //return UV index
+                console.log()
+            })
+        }else{
+            alert("error: "+ response.statusText);
+        }
+    })
+}
+
+var getCityWeatherFive = function(city){
+    var apiUrlFive = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=f70bac3dc5426bfdb76e72707800686a`
+
+    //make a request to the url
+    fetch(apiUrlFive)
+    .then(function(response){
+        if(response.ok){
+            console.log(response);
+            response.json().then(function(data){
+                console.log(data);
+                //return next day noon date
+                console.log(data.list[3].dt_txt)
+                //return next day noon image
+                console.log(data.list[3].weather[0].icon)
+                //return next day noon temp
+                console.log((data.list[3].main.temp-273.15)*(9/5)+32)
+                //return next day noon wind
+                console.log(data.list[3].wind.speed)
+                //return next day noon humidity
+                console.log(data.list[3].main.humidity)
             })
         }else{
             alert("error: "+ response.statusText);
